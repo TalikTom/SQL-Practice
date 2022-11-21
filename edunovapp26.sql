e771420d8d84246c2abe02f27596de63083afaca
Luka Agic <lukagic@gmail.com>
2022-11-21 22:30:03 +0100

    Create relationships between tables

--- edunovapp26.sql
+++ edunovapp26.sql
@@ -44,3 +44,12 @@ create table clan(
     grupa int not null,
     polaznik int not null
 );
+
+alter table grupa add foreign key (smjer) references smjer (sifra);
+alter table grupa add foreign key (predavac) references predavac (sifra);
+
+alter table clan add foreign key (grupa) references grupa (sifra);
+alter table clan add foreign key (polaznik) references polaznik (sifra);
+
+alter table polaznik add foreign key (osoba) references osoba (sifra);
+alter table predavac add foreign key (osoba) references osoba (sifra);
