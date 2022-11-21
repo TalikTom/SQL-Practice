c424282f931a29bd1b2b9da557b53d6d357819a6
Luka Agic <lukagic@gmail.com>
2022-11-21 22:27:09 +0100

    Create tables

--- edunovapp26.sql
+++ edunovapp26.sql
@@ -1,3 +1,46 @@
 drop database if exists edunovapp26;
 create database edunovapp26 default charset utfmb4;
 use edunovapp26;
+
+create table smjer(
+    sifra int not null primary key auto_increment,
+    naziv varchar(50) not null,
+    cijena decimal(18,2),
+    upisnina decimal(18,2),
+    trajanje int not null,
+    certificiran boolean not null
+):
+
+create table grupa(
+    sifra int not null primary key auto_increment,
+    naziv varchar(50) not null,
+    maksimalnopolaznika int not null,
+    datumpocetka datetime,
+    smjer int not null,
+    predavac int
+);
+
+create table osoba(
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    email varchar(50) not null,
+    oib char(11)
+);
+
+create table polaznik(
+    sifra int not null primary key auto_increment,
+    iban varchar(50),
+    osoba int not null
+);
+
+create table predavac(
+    sifra int not null primary key auto_increment,
+    iban varchar(50),
+    osoba int not null
+)
+
+create table clan(
+    grupa int not null,
+    polaznik int not null
+);
