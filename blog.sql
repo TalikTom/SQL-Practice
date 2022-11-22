a43215258af52897f8998172cb3acf5342ad060d
Luka Agic <lukagic@gmail.com>
2022-11-22 13:01:07 +0100

    Add attributes

--- blog.sql
+++ blog.sql
@@ -3,9 +3,12 @@ create database blog;
 use blog;
 
 create table posts(
-    id INT auto_increment primary key
+    id INT auto_increment primary key not null,
+    title varchar(50) not null
 );
 
 create table comments(
-    id INT auto_increment primary key
+    id INT auto_increment primary key not null,
+    body text,
+    post_id int not null
 );
