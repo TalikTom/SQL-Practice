35eecd1fe049b2f27a34384704b81d8bfc2f1be2
Luka Agic <lukagic@gmail.com>
2022-11-22 23:25:27 +0100

    Update company.sql with tables

--- company.sql
+++ company.sql
@@ -9,3 +9,22 @@ create table employees(
     last_name varchar(50),
     hire_date date
 );
+
+create table departments(
+    id int not null auto_increment primary key,
+    dept_name varchar(50)
+);
+
+create table salaries(
+    id int not null auto_increment primary key,
+    salary decimal(13,2),
+    from_date date,
+    to_date date
+);
+
+create table title(
+    id int not null auto_increment primary key,
+    title varchar(50),
+    from_date date,
+    to_date date
+)
