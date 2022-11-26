a7f133227a085f76068fa21603e03ea92a47f93b
Luka Agic <lukagic@gmail.com>
2022-11-26 23:58:35 +0100

    Refactor code

--- saqila practice sql.sql
+++ saqila practice sql.sql
@@ -28,9 +28,11 @@ select
 from customer
 left join rental
 on rental.customer_id  = customer.customer_id
-left join address 
-on address.address_id = (
-	select address_id from store where store.store_id = customer.store_id 	
-)
+left join store 
+on store.store_id = customer.store_id
+left join address
+on address.address_id = store.address_id 
 group by customer.customer_id, address.address;
+
+
 
