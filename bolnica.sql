4e6f6068348faeb35fa33c4faf0e4a70fe2a8f02
Luka Agic <lukagic@gmail.com>
2022-11-28 17:02:02 +0100

    Create tables

--- bolnica.sql
+++ bolnica.sql
@@ -8,3 +8,38 @@ create table odjel (
     broj_soba int
 );
 
+create table pacijent (
+    pacijent_id int not null auto_increment primary key,
+    ime varchar(50),
+    prezime varchar(50),
+    adresa varchar(255),
+    oib char(11)
+);
+
+create table doktor (
+    doktor_id pacijent_id int not null auto_increment primary key,
+    ime varchar(50),
+    prezime varchar(50),
+    specijalizacija varchar(50),
+    oib char(11)
+);
+
+create table medicinski_karton (
+    medicinski_karton_id int not null auto_increment primary key,
+    datum_pregleda datetime,
+    dijagnoza varchar(255),
+);
+
+create table posjeta (
+    posjeta_id int not null auto_increment primary key,
+    datum datetime
+);
+
+create table posjetitelj (
+    posjetitelj_id int not null auto_increment primary key,
+    ime varchar(50),
+    prezime varchar(50),
+    adresa varchar(50),
+    telefonski_broj int
+);
+
