c73e25b139019763563a831d47df5916eaf07210
Luka Agic <lukagic@gmail.com>
2022-12-01 23:34:21 +0100

    Vjezba5 Mysql

--- vjezba_kolokvij1.sql
+++ vjezba_kolokvij1.sql
@@ -143,3 +143,19 @@ where kuna > 15.78;
 
 select kratkamajica from zena
 where hlace like '%ana%';
+
+select a.dukserica, f.asocijalno, e.hlace
+from svekar a
+inner join sestra_svekar b
+on a.sifra = b.svekar
+inner join sestra c
+on c.sifra = b.sestra
+inner join zena d 
+on d.sestra = c.sifra 
+inner join muskarac e 
+on e.zena = d.sifra 
+inner join mladic f 
+on f.muskarac = e.sifra
+where d.hlace like 'a%'
+and c.haljina like '%ba%'
+order by e.hlace desc;
