80d66cde26fe6160af9968981f9a038c2832f4b7
Luka Agic <lukagic@gmail.com>
2022-12-08 23:23:35 +0100

    Update bolnica.sql

--- bolnica.sql
+++ bolnica.sql
@@ -37,7 +37,8 @@ create table posjeta (
     posjeta_id int not null auto_increment primary key,
     datum datetime,
     pacijent_id int,
-    posjetitelj_id int
+    posjetitelj_id int,
+    max_trajanje
 );
 
 create table posjetitelj (
@@ -56,3 +57,17 @@ alter table posjeta add foreign key (posjetitelj_id) references posjetitelj(posj
 alter table medicinski_karton add foreign key (pacijent_id) references pacijent(pacijent_id);
 
 alter table doktor add foreign key (odjel_id) references odjel(odjel_id);
+
+insert into odjel (naziv, broj_soba)
+values ('Kardiologija', 15);
+insert into odjel (naziv, broj_soba)
+values ('Psihijatrija', 1);
+insert into odjel (naziv, broj_soba)
+values ('Neurologija', 5);
+
+insert into pacijent (ime, prezime, adresa, oib, doktor_id)
+values ('Marko', 'Marulic', 'Trosinska 21, Silas 31011', 15487569851, 1);
+insert into pacijent (ime, prezime, adresa, oib, doktor_id)
+values ('Jovica', 'Jovic', 'B.B. Billmayera 41, Osijek 31000', 15487569851, 3);
+insert into pacijent (ime, prezime, adresa, oib, doktor_id)
+values ('Sinan', 'Sakic', ' 21, Ernestinovo 31456', 15487239851, 2);
