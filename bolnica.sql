d3542dcfdeb21ec138312c5d1a8713d745b78436
Luka Agic <lukagic@gmail.com>
2022-12-09 22:25:13 +0100

    Insert data into bolnica.sql

--- bolnica.sql
+++ bolnica.sql
@@ -38,7 +38,7 @@ create table posjeta (
     datum datetime,
     pacijent_id int,
     posjetitelj_id int,
-    max_trajanje
+    max_trajanje_minute int
 );
 
 create table posjetitelj (
@@ -65,9 +65,40 @@ values ('Psihijatrija', 1);
 insert into odjel (naziv, broj_soba)
 values ('Neurologija', 5);
 
+
+
+insert into doktor (ime, prezime, specijalizacija, oib, odjel_id)
+values('Imzo', 'Prezimic', 'Kardiologija', 45798462514, 1);
+insert into doktor (ime, prezime, specijalizacija, oib, odjel_id)
+values('Hrvoje', 'Samic', 'Psihijatrija', 45798212514, 2);
+insert into doktor (ime, prezime, specijalizacija, oib, odjel_id)
+values('Gojka', 'Prezimic', 'Neurologija', 45298462514, 3);
+
 insert into pacijent (ime, prezime, adresa, oib, doktor_id)
 values ('Marko', 'Marulic', 'Trosinska 21, Silas 31011', 15487569851, 1);
 insert into pacijent (ime, prezime, adresa, oib, doktor_id)
 values ('Jovica', 'Jovic', 'B.B. Billmayera 41, Osijek 31000', 15487569851, 3);
 insert into pacijent (ime, prezime, adresa, oib, doktor_id)
 values ('Sinan', 'Sakic', ' 21, Ernestinovo 31456', 15487239851, 2);
+
+insert into medicinski_karton (datum_pregleda, dijagnoza, pacijent_id)
+values ('2022-09-09', "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley", 1);
+insert into medicinski_karton (datum_pregleda, dijagnoza, pacijent_id)
+values ('2022-08-08', "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard", 2);
+insert into medicinski_karton (datum_pregleda, dijagnoza, pacijent_id)
+values ('2022-12-12', "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.", 3);
+
+insert into posjetitelj (ime, prezime, adresa, telefonski_broj)
+values ('Posjet', 'Kasnic', 'Ul. Inatise 76, Zupanja 32260', '098784652');
+insert into posjetitelj (ime, prezime, adresa, telefonski_broj)
+values ('Lakomisl', 'Trazic', 'Idarisa Sadrisa 2, Osijek 31000', '098782352');
+insert into posjetitelj (ime, prezime, adresa, telefonski_broj)
+values ('Butro', 'Rasnic', 'Trg Slobode 1, Zupanja 32260', '098784642');
+
+insert into posjeta (datum, pacijent_id, posjetitelj_id, max_trajanje_minute)
+values ('2022-12-09', 1, 1, 15);
+insert into posjeta (datum, pacijent_id, posjetitelj_id, max_trajanje_minute)
+values ('2022-12-08', 2, 2, 15);
+insert into posjeta (datum, pacijent_id, posjetitelj_id, max_trajanje_minute)
+values ('2022-12-07', 3, 3, 15);
+
